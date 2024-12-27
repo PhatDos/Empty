c4269e5925a481878ddcac826616c90947ba0760
PhatDos <162654433+PhatDos@users.noreply.github.com>
2024-12-27 22:18:00 +0700

    CHINH

--- first.asm
+++ first.asm
@@ -1,12 +1,12 @@
 
-len equ $-hello
-section .text
+len equ $-hello sdaksjdnakdaskjlkkadkdasdlks
+section .textasdasdasdsadas
 global _start
 _start:
-mov al, [a]
-add al, [b]
-mov [c], al
-mov eax, 4
+mov al, [a]aasdasd
+add al, [b]asdas
+mov [c], aldas
+mov eax, 4das
 mov ebx, 1
 mov ecx, hello
 mov edx, len
